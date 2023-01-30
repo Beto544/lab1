327647b9cb5c93b7d6d7d7e20a78304c67121bbc
Beto544 <hunberto.pc544@gmail.com>
2023-01-30 15:11:24 -0800

    first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+//Hunberto Pascual
+//lab-1 jan 24
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = rand() % 10 + 1;
+    cout<< a << endl;
+    cout << "Hello world\n ";
+    // local change
+    // 2nd change
+    // 3rd change
+    return 0;
+}
+
+
